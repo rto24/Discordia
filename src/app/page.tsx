a90b8308ba053baca7314536e10ea20e0ac7ebd6
rto24 <richardto180@gmail.com>
2024-10-18 14:38:10 -0400

    added reusable button from shadcn

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,15 @@
+import { Button } from '@/components/ui/button'
 import React from 'react'
 
 const Home = () => {
   return (
-    <div>Homepage</div>
+    <div>
+      <section>
+        <h1>Homepage</h1>
+        <Button variant="outline">Button</Button>
+      </section>
+    </div>
+    
   )
 }
 
