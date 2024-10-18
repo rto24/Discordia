d7e63f0cd6896caac882591e6709c3d3418181e1
rto24 <richardto180@gmail.com>
2024-10-18 13:57:36 -0400

    initial setup and DB connection

---
+++ server/server.ts
@@ -0,0 +1,16 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`)
+})
