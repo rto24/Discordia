985b4fe72485cd830d4d4fb8b96ed971c7aca64e
rto24 <richardto180@gmail.com>
2024-10-20 15:13:10 -0400

    functions to create and find users

--- server/config/db.ts
+++ server/config/db.ts
@@ -7,4 +7,15 @@ const pool = new Pool({
   connectionString: process.env.DATABASE_URI,
 });
 
+export async function findUser(discordId: string) {
+  const result = await pool.query(`SELECT * FROM Person WHERE discord_id = $1`, [discordId]);
+  return result.rows[0]; //return existing user info
+}
+
+export async function createUser(discordId: string, username: string, email: string) {
+  const query = `INSERT INTO Person (discord_id, username, email) VALUES ($1 $2 $3) RETURNING *`;
+  const result = await pool.query(query, [discordId, username, email]);
+  return result.rows[0]; //return new user added
+}
+
 export default pool;
