65fca1574dfab4d772e3da30db4dc0529c0bb271
rto24 <richardto180@gmail.com>
2024-10-20 15:14:28 -0400

    disc oauth passport config

---
+++ server/config/passportConfig.ts
@@ -0,0 +1,49 @@
+import passport from 'passport';
+import { Strategy as DiscordStrategy } from 'passport-discord';
+import { Profile } from 'passport-discord';
+import { findUser, createUser } from './db';
+import { VerifyCallback } from 'passport-oauth2';
+import { User } from '../types/types';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+passport.use(new DiscordStrategy({
+  clientID: process.env.DISCORD_CLIENT_ID!,
+  clientSecret: process.env.DISCORD_CLIENT_SECRET!,
+  callbackURL: process.env.DISCORD_REDIRECT_URI!,
+  scope: ['identify', 'email', 'guilds']
+},
+  async (accessToken: string, refreshToken: string, profile: Profile, done: VerifyCallback) => {
+    try {
+      const existingUser = await findUser(profile.id);
+      if (existingUser) {
+        return done(null, existingUser);
+      } else {
+        const newUser = await createUser(profile.id, profile.username, profile.email || "");
+        return done(null, newUser);
+      }
+    } catch (err) {
+      console.error('Error in DiscordStrategy:', err);
+      return done(err)
+    }
+  }
+));
+
+passport.serializeUser((user: User, done) => {
+  done(null, user.id); 
+});
+
+passport.deserializeUser((id: string, done) => {
+  findUser(id)
+    .then((user) => {
+      if (user) {
+        done(null, user);  
+      } else {
+        done(null, null); 
+      }
+    })
+    .catch((err) => {
+      done(err, null);  
+    });
+});
