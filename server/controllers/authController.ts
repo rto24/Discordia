443afa8f8c13e7e98362080c93c85b94522bcb89
rto24 <richardto180@gmail.com>
2024-10-29 14:23:12 -0400

    jwt functionality and added authentication middleware

--- server/controllers/authController.ts
+++ server/controllers/authController.ts
@@ -1,26 +1,44 @@
 import { Response, Request } from 'express';
+import jwt from 'jsonwebtoken';
+import { User } from '../types/types';
 
 export const successfulLogin = (req: Request, res: Response) => {
-  res.redirect('http://localhost:3000/home');
+  if (req.user) {
+    const user = req.user as User;
+    const tokenPayload = { id: user.id, discord_id: user.discordId, username: user.username }
+    const token = jwt.sign(
+      tokenPayload,
+      process.env.JWT_SECRET!,
+      { expiresIn: '1h' }
+    );
+    res.cookie('auth_token', token, { httpOnly: true, secure: true });
+    res.redirect('http://localhost:3000/home');
+  } else {
+    res.status(401).json({ message: 'Login failed' })
+  }
 };
 
 export const failedLogin = (req: Request, res: Response) => {
   res.status(401).json({message: 'Login failed'})
 };
 
-export const checkAuthStatus = (req: Request, res: Response) => {
-  if (req.isAuthenticated()) {
-    res.json({ authenticated: true, user: req.user });
-  } else {
+export const checkAuthStatus = (req: Request, res: Response): void => {
+  const token = req.cookies['auth_token'];
+  if (!token) {
     res.json({ authenticated: false });
+    return;
+  }
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET!) as User;
+    res.json({ authenticated: true, user: decoded });
+  } catch (error) {
+    res.status(401).json({ authenticated: false, message: error });
+    return;
   }
 };
 
 export const logout = (req: Request, res: Response) => {
-  req.logout((err) => {
-    if (err) {
-      return res.status(500).json({ message: 'Logout failed' });
-    }
-    res.redirect('http://localhost:3000');
-  });
+  res.clearCookie('auth_token');
+  res.redirect('http://localhost:3000');
 };
--- server/routes/authRoutes.ts
+++ server/routes/authRoutes.ts
@@ -1,12 +1,13 @@
 import { Router } from 'express';
 import passport from '../config/passportConfig';
 import { successfulLogin, checkAuthStatus, logout } from '../controllers/authController';
+import { authenticateJWT } from '../middleware/authMiddleware';
 
 const router = Router();
 
 router.get('/discord', passport.authenticate('discord'));
 router.get('/discord/callback', passport.authenticate('discord', { failureRedirect: '/' }), successfulLogin);
-router.get('/status', checkAuthStatus);
+router.get('/status', authenticateJWT, checkAuthStatus);
 router.get('/logout', logout);
 
 export default router;
