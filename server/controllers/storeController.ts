46232539af8d9a979b7fa8759a6f83b02ff8be1f
rto24 <richardto180@gmail.com>
2024-11-01 13:01:43 -0400

    added controller and route for user currency

--- server/controllers/storeController.ts
+++ server/controllers/storeController.ts
@@ -7,14 +7,31 @@ import pool from "../config/db";
       const queryString: string = 'SELECT * FROM Store';
       const result = await pool.query(queryString);
       res.locals.items = result.rows;
-      next();
+      return next();
     } catch (error) {
-      next(error)
+      return next(error)
+    }
+  };
+
+  const getCurrency: RequestHandler = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const { username } = req.body;
+      const queryString: string = `SELECT currency FROM Person WHERE username = $1`;
+      const result = await pool.query(queryString, [username]);
+      if (result.rows.length > 0 && result.rows[0].currency !== undefined) {
+        res.locals.currency = result.rows[0].currency;
+        return next();
+      } else {
+        return next({ message: "User not found"});
+      }
+    } catch (error) {
+      return next(error)
     }
   };
 
   const storeController = {
     getItems,
+    getCurrency
   }
 
 
--- server/routes/storeRoutes.ts
+++ server/routes/storeRoutes.ts
@@ -7,4 +7,8 @@ router.get('/items', storeController.getItems, (req: Request, res: Response): vo
   res.status(200).json(res.locals.items)
 });
 
+router.post('/currency', storeController.getCurrency, (req: Request, res: Response): void => {
+  res.status(200).json(res.locals.currency)
+});
+
 export default router;
