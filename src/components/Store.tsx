ff502af7f692f6e50c2250cb793d531d893ef096
rto24 <richardto180@gmail.com>
2024-11-01 18:26:19 -0400

    styled store page

--- src/components/Store.tsx
+++ src/components/Store.tsx
@@ -3,6 +3,7 @@ import { storeItem } from '@/types/types';
 import Image from 'next/image';
 import { Button } from './ui/button';
 import { useUser } from '@/context/UserContext';
+import { CardContainer } from './ui/card';
 
 const ItemShop = () => {
   const [items, setItems] = useState<storeItem[]>([]);
@@ -76,28 +77,48 @@ const ItemShop = () => {
   }, [token]);
 
   return (
-    <div>
+    <div className="flex justify-center w-full">
       <h1>Item Shop</h1>
       <h2>{`${currency}`}</h2>
-      <div className="grid gap-4 grid-cols-5 grid-rows-4">
-        {items.map((item) => {
-          item.name = item.name.toLowerCase().replace(" ", "-");
-          const imagePath = `/${item.name}.png`;
+      <div className="grid gap-x-20 gap-y-1 grid-cols-5 auto-rows-fr">
+        {items.map((item, index) => {
+          const formattedItemName = item.name.toLowerCase().replace(/\s+/g, "-");
+          const imagePath = `/${formattedItemName}.png`;
+
+          const colorMap = {
+            "Common": "text-gray-300 shadow-[0_0_30px_10px_rgba(209,213,219,0.8)]", 
+            "Rare": "text-blue-400 shadow-[0_0_30px_10px_rgba(96,165,250,0.8)]", 
+            "Epic": "text-purple-500 shadow-[0_0_30px_10px_rgba(168,85,247,0.8)]", 
+            "Legendary": "text-orange-500 shadow-[0_0_30px_10px_rgba(249,115,22,0.8)]"
+          } 
+    
+          const tierColor = colorMap[item.tier];
+
           return (
-            <div className="flex flex-col" key={item.id}>
-              <h2>{item.name}</h2>
-              <Image 
-                src={imagePath}
-                alt={item.name}
-                width={200}
-                height={200}
-              />
-              <p>Price: {item.price}</p>
-              <p>{item.tier}</p>
-              <p>{item.type}</p>
-              <p>{item.description}</p>
-              <Button>Purchase</Button>
-            </div>
+            <CardContainer
+              key={index}
+              className={`text-white p-4 rounded-lg bg-zinc-900 flex flex-col items-center h-full w-full transition-all duration-300 ${tierColor} hover:shadow-lg hover:${colorMap[item.tier].split(' ')[1]}`}
+              containerClassName="w-[350px] h-[600px]"
+            >
+              <div className="flex flex-col items-center justify-center mb-4">
+                <Image 
+                  src={imagePath}
+                  alt={item.name}
+                  width={200}
+                  height={200}
+                />
+                <h3 className="text-lg font-semibold text-white">
+                  {item.name}
+                </h3>
+                <h3 className={`font-semibold ${tierColor.split(' ')[0]} text-center`}>{item.tier}</h3>
+                <p className="text-sm text-gray-400 text-center mb-5">{item.description}</p>
+                <Button
+                  className="outline outline-1"
+                >
+                  Purchase
+                </Button>
+              </div>
+            </CardContainer>
           )
         })}
       </div>
