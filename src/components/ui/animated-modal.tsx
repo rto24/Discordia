bf402b252c0b856b976a03d349138ab48647c50b
rto24 <richardto180@gmail.com>
2024-11-06 14:35:43 -0500

    ui element for modal

---
+++ src/components/ui/animated-modal.tsx
@@ -0,0 +1,219 @@
+"use client";
+import { cn } from "@/lib/utils";
+import { AnimatePresence, motion } from "framer-motion";
+import React, {
+  ReactNode,
+  createContext,
+  useContext,
+  useEffect,
+  useRef,
+  useState,
+} from "react";
+
+interface ModalContextType {
+  open: boolean;
+  setOpen: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+const ModalContext = createContext<ModalContextType | undefined>(undefined);
+
+export const ModalProvider = ({ children }: { children: ReactNode }) => {
+  const [open, setOpen] = useState(false);
+
+  return (
+    <ModalContext.Provider value={{ open, setOpen }}>
+      {children}
+    </ModalContext.Provider>
+  );
+};
+
+export const useModal = () => {
+  const context = useContext(ModalContext);
+  if (!context) {
+    throw new Error("useModal must be used within a ModalProvider");
+  }
+  return context;
+};
+
+export function Modal({ children }: { children: ReactNode }) {
+  const { open } = useModal();
+  return open ? <>{children}</> : null;
+}
+
+export const ModalTrigger = ({
+  children,
+  className,
+  onClick,
+}: {
+  children: ReactNode;
+  className?: string;
+  onClick?: () => void;
+}) => {
+  const { setOpen } = useModal();
+  return (
+    <button
+      className={cn(
+        "px-4 py-2 rounded-md text-white dark:text-white text-center relative overflow-hidden z-50",
+        className
+      )}
+      onClick={() => {
+        setOpen(true);
+        if (onClick) {
+          onClick();
+        }
+      }}
+    >
+      {children}
+    </button>
+  );
+};
+
+export const ModalBody = ({
+  children,
+  className,
+}: {
+  children: ReactNode;
+  className?: string;
+}) => {
+  const { open } = useModal();
+  const modalRef = useRef(null);
+  const { setOpen } = useModal();
+
+  useEffect(() => {
+    if (open) {
+      document.body.style.overflow = "hidden";
+    } else {
+      document.body.style.overflow = "auto";
+    }
+
+    return () => {
+      document.body.style.overflow = "auto"; // Ensure scrolling is restored
+    };
+  }, [open]);
+
+  useOutsideClick(modalRef, () => setOpen(false));
+
+  return (
+    <AnimatePresence>
+      {open && (
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          exit={{ opacity: 0 }}
+          className="fixed inset-0 flex items-center justify-center z-50"
+        >
+          <Overlay />
+
+          <motion.div
+            ref={modalRef}
+            className={cn(
+              "w-[90%] md:w-[600px] h-[400px] bg-white dark:bg-neutral-950 border dark:border-neutral-800 shadow-lg rounded-lg p-6 relative z-50",
+              className
+            )}
+            initial={{ opacity: 0, scale: 0.5, y: 40 }}
+            animate={{ opacity: 1, scale: 1, y: 0 }}
+            exit={{ opacity: 0, scale: 0.9 }}
+            transition={{ type: "spring", stiffness: 260, damping: 20 }}
+          >
+            <CloseIcon />
+            {children}
+          </motion.div>
+        </motion.div>
+      )}
+    </AnimatePresence>
+  );
+};
+
+export const ModalContent = ({
+  children,
+  className,
+}: {
+  children: ReactNode;
+  className?: string;
+}) => {
+  return (
+    <div className={cn("flex flex-col flex-1 p-4", className)}>
+      {children}
+    </div>
+  );
+};
+
+export const ModalFooter = ({
+  children,
+  className,
+}: {
+  children: ReactNode;
+  className?: string;
+}) => {
+  return (
+    <div
+      className={cn(
+        "flex justify-end p-4 bg-gray-100 dark:bg-neutral-900 rounded-b-lg",
+        className
+      )}
+    >
+      {children}
+    </div>
+  );
+};
+
+const Overlay = ({ className }: { className?: string }) => {
+  return (
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1, backdropFilter: "blur(5px)" }}
+      exit={{ opacity: 0 }}
+      className={`fixed inset-0 h-full w-full bg-black bg-opacity-50 ${className}`}
+    ></motion.div>
+  );
+};
+
+const CloseIcon = () => {
+  const { setOpen } = useModal();
+  return (
+    <button
+      onClick={() => setOpen(false)}
+      className="absolute top-4 right-4 z-50 text-gray-700 hover:text-gray-900 dark:text-gray-300 dark:hover:text-gray-100"
+    >
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        className="h-6 w-6"
+      >
+        <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+        <path d="M18 6l-12 12" />
+        <path d="M6 6l12 12" />
+      </svg>
+    </button>
+  );
+};
+
+// Hook to detect clicks outside of a component.
+export const useOutsideClick = (
+  ref: React.RefObject<HTMLDivElement>,
+  callback: (event: MouseEvent | TouchEvent) => void
+) => {
+  useEffect(() => {
+    const listener = (event: MouseEvent | TouchEvent) => {
+      if (!ref.current || ref.current.contains(event.target as Node)) {
+        return;
+      }
+      callback(event);
+    };
+
+    document.addEventListener("mousedown", listener);
+    document.addEventListener("touchstart", listener);
+
+    return () => {
+      document.removeEventListener("mousedown", listener);
+      document.removeEventListener("touchstart", listener);
+    };
+  }, [ref, callback]);
+};
