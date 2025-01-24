29245b9839ac758b76eebe048491ebe44ef2dfc1
rto24 <richardto180@gmail.com>
2025-01-23 23:12:54 -0500

    separated item name, tier, and type for dropdown filters

--- src/components/FilterBar.tsx
+++ src/components/FilterBar.tsx
@@ -1,7 +1,7 @@
 "use client"
 
-import React, { useState } from "react";
-import { storeItem } from "@/types/types";
+import React, { useState, useMemo, useEffect } from "react";
+import { storeItem, StoreSearchParams } from "@/types/types";
 
 interface FilterBarProps {
   items: storeItem[];
@@ -9,8 +9,29 @@ interface FilterBarProps {
 }
 
 const FilterBar = ({ items, onFilter }: FilterBarProps) => {
-  const [ searchParams, setSearchParams ] = useState({ term: "", filters: {} })
-  
+  const [ searchParams, setSearchParams ] = useState<StoreSearchParams>({ term: "", filters: {} })
+
+  const itemNames = useMemo(() => {
+    return Array.from(new Set(items.map((item) => item.name)))
+  }, [items]);
+
+  const tiers = useMemo(() => {
+    return Array.from(new Set(items.map((item) => item.tier)))
+  }, [items]);
+
+  const types = useMemo(() => {
+    return Array.from(new Set(items.map((item) => item.type)))
+  }, [items]);
+
+  useEffect(() => {
+    console.log(itemNames, tiers, types)
+  }, [itemNames, tiers, types])
+
+  return (
+    <div>
+      Search yay
+    </div>
+  )
 };
 
 export default FilterBar;
